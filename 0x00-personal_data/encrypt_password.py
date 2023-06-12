4a410cb65616be247f26102d29fc031245500fcf
Mkiige <mkiige77@gmail.com>
2023-06-12 19:00:49 +0300

    done

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,31 +1,16 @@
 #!/usr/bin/env python3
-"""
-Module encrypt_password
+"""A module for encrypting passwords.
 """
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """Reterive a salted, hashed password, which is a byte string.
-    Parameters
-    ----------
-    password: str
-      password to be hashed
+    """Returns a salted, hashed password, which is a byte string.
     """
-    encode = bytes(password, encoding="utf-8")
-    hashed = bcrypt.hashpw(encode, bcrypt.gensalt())
-    return hashed
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """validate that the provided password matches the hashed password
-
-    Parameters
-    ----------
-    hashed_password: bytes
-        hashed password
-    password: str
-      password to be hashed
+    """Checks if a provided password matches a hashed password.
     """
-    encode = bytes(password, encoding="utf-8")
-    return bcrypt.checkpw(encode, hashed_password)
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
